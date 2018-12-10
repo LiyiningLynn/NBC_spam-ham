8a22d151c69886476895bdb80e6b585766b3a1a5
Li <42381073+LiyiningLynn@users.noreply.github.com>
2018-12-10 15:52:13 +0900

    Update nbc_classification.py

--- nbc_classification.py
+++ nbc_classification.py
@@ -10,4 +10,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import csv
+
+def loadCsv(filename):
+  lines = csv.reader(open(filename,"rb"))
+  dataset = list(lines)
+  for i in range(len(dataset)):
+    dataset[i] = [float(x) for x in dataset[i]]
+  return dataset
+'''
+preprocess
+'''
+filename = 'pima-indians-diabetes.data.csv'
+dataset = loadCsv(filename)
+print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
+
 def read_file(filename):
